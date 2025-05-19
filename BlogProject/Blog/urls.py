b27a5eff8f459f4a7341efef437b14c7be049eb7
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2025-05-19 02:25:23 -0500

    Set up homepage
    Added a home button, a login and sign up button. I also added a background image.

--- BlogProject/Blog/urls.py
+++ BlogProject/Blog/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, LoginView, SignUpView
 
 urlpatterns = [
     path('', HomeView.as_view(), name = 'home'),
+    path('login/', LoginView.as_view(), name = 'login'),
+    path('signup/', SignUpView.as_view(), name = 'signUp'),
 ]
--- BlogProject/Blog/views.py
+++ BlogProject/Blog/views.py
@@ -6,3 +6,11 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         return render(request, 'home.html')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+class SignUpView(View):
+    def get(self, request):
+        return render(request, 'signup.html')
