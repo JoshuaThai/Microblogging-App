bf6c722a5be9b3e3901ea2e96399c636737029fd
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2025-05-20 23:37:44 -0500

    Added additional tests to test settings feature
    Added more tests and refactored some tests

--- BlogProject/Blog/tests/test_settings.py
+++ BlogProject/Blog/tests/test_settings.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.test import TestCase, Client
+from django.urls import reverse
 
 from ..models import User
 
@@ -19,16 +20,15 @@ from ..models import User
 class SettingsUnsuccessfulTest(TestCase):
     def setUp(self):
         self.client = Client()
-        user = User(
+        self.user = User.objects.create_user(
             first_name='John',
             last_name='Doe',
             username='testuser',
             email='email@gmail.com',
             birthDate=datetime.date(1970, 1, 1),
-            role='user'
+            role='user',
+            password='password'
         )
-        user.set_password('password')
-        user.save()
 
 
         user1 = User(
@@ -41,39 +41,38 @@ class SettingsUnsuccessfulTest(TestCase):
         )
         user1.set_password('password')
         user1.save()
+        login_successful = self.client.login(username='testuser', password='password')
+        print("Login success?", login_successful)
 
     def test_user_model_exists(self):
         self.assertTrue(User.objects.filter(username='testuser').exists())
-    def test_user_log_in(self):
-        self.client.login(username='testuser', password='password')
     # first name cannot be changed to empty string
     def test_first_name_empty(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeFirst',
             'first_name': ''
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'First name cannot be empty. Enter your new first name.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.first_name != '') # should not be changed.
+
+        self.assertTrue(self.user.first_name != '') # should not be changed.
 
     def test_last_name_empty(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeLast',
             'last_name': ''
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Last name cannot be empty. Enter your new last name.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.last_name != '')  # should not be changed.
+        self.assertTrue(self.user.last_name != '')  # should not be changed.
 # username checks
     def test_username_empty(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeUsername',
             'username': ''
         })
 
@@ -81,93 +80,124 @@ class SettingsUnsuccessfulTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Username cannot be empty. '
                                       'Enter a valid new username.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.username != '')  # should not be changed.
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.username != '')  # should not be changed.
 
     def test_username_short(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeUsername',
             'username': 'test'
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Username should be atleast 5 characters. Enter a valid new username.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.username != 'test')  # should not be changed.
+        self.assertContains(response, 'Username should be at least 5 characters. Enter a valid new username.')
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.username != 'test')  # should not be changed.
 
 # password checks
-    # no password at all
-    def test_noPassword(self):
-        client = Client()
-        response = client.post('/settings/', {
-            'password': ''
+    # old password not entered
+    def test_no_old_Password(self):
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changePass',
+            'oldPass': '',
+            'newPass': 'password'
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Password cannot be blank. Enter a valid new password.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.password != '')  # should not be changed.
+        self.assertContains(response, 'Old password cannot be empty. Please enter your old password.')
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.password != '')  # should not be changed.
+
+    # old password not entered
+    def test_no_new_Password(self):
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changePass',
+            'oldPass': 'password',
+            'newPass': ''
+        })
+
+        # page should be rendered or reloaded as settings was invalid.
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New password cannot be blank. Please enter a valid new password.')
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.password != '')  # should not be changed.
 
 
         # too short of a password
     def test_shortPassword(self):
-        client = Client()
-        response = client.post('/settings/', {
-            'password': 'e'
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changePass',
+            'oldPass': 'password',
+            'newPass': 'pass'
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,
                             'Password too short! Please enter a new password that is at least 8 characters.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.password != 'e')  # should not be changed.
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.password != 'e')  # should not be changed.
         # too long of a password
     def test_longPassword(self):
-        client = Client()
-        response = client.post('/settings/', {
-            'password': '1234567890123456789012345',
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changePass',
+            'oldPass': 'password',
+            'newPass': '1234567890123456789012345',
         })
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,
                             'Password too long! Please enter a new password that is at most 24 characters.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.password != '1234567890123456789012345')  # should not be changed.
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.password != '1234567890123456789012345')  # should not be changed.
 
+    def test_incorrect_old_password(self):
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changePass',
+            'oldPass': 'greater52',
+            'newPass': 'password123'
+        })
+
+        # page should be rendered or reloaded as settings was invalid.
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,'The old password entered does not match our records. '
+                                     'Please re-enter your old password.')
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.password != 'password123')  # should not be changed.
 
 # email checks
     def test_noEmail(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action':'changeEmail',
             'email': '',
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Email cannot be empty. Enter a valid email.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.email != '')  # should not be changed.
+
+        self.assertTrue(self.user.email != '')  # should not be changed.
 
     # invalid email address
     def test_invalidEmail(self):
-        client = Client()
-        response = client.post('/settings/', {
+
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action':'changeEmail',
             'email': 'johnnymail.com',
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Invalid email address. Enter a valid email address.')
-        findUser = User.objects.filter(username='testuser').first()
-        self.assertTrue(findUser.email != 'johnnymail.com')  # should not be changed.
+        self.assertTrue(self.user.email != 'johnnymail.com')  # should not be changed.
 
     # enter an email address that is already used
     def test_uniqueEmail(self):
-        client = Client()
-        response = client.post('/settings/', {
+
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action':'changeEmail',
             'email': 'email@outlook.com',
         })
 
@@ -175,35 +205,33 @@ class SettingsUnsuccessfulTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'This email is already associated with another account. '
                             'Please enter a different email address.')
-        find_user = User.objects.filter(username='testuser').first()
-        self.assertTrue(find_user.email != 'email@outlook.com')  # should not be changed.
+
+        self.assertTrue(self.user.email != 'email@outlook.com')  # should not be changed.
 
     # Not entering an date of birth
     def test_noDate(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action':'changeDate',
             'birthDate': ''
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Please enter your date of birth.')
-        find_user = User.objects.filter(username='testuser').first()
-        self.assertTrue(find_user.birthDate != '')  # should not be changed.
+        self.assertContains(response, 'Date of birth cannot be empty. Please enter your date of birth.')
+        self.assertTrue(self.user.birthDate != '')  # should not be changed.
 
 class SettingsSuccessfulTest(TestCase):
     def setUp(self):
         self.client = Client()
-        user = User(
+        self.user = User.objects.create_user(
             first_name='John',
             last_name='Doe',
             username='testuser',
             email='email@gmail.com',
             birthDate=datetime.date(1970, 1, 1),
-            role='user'
+            role='user',
+            password='password'
         )
-        user.set_password('password')
-        user.save()
 
         user1 = User(
             first_name='Jane',
@@ -215,97 +243,108 @@ class SettingsSuccessfulTest(TestCase):
         )
         user1.set_password('password')
         user1.save()
+        login_successful = self.client.login(username='testuser', password='password')
+        print("Login success?", login_successful)
 
     def test_user_model_exists(self):
         self.assertTrue(User.objects.filter(username='testuser').exists())
 
-    def test_user_log_in(self):
-        self.client.login(username='testuser', password='password')
-
     # first name cannot be changed to empty string
     def test_first_name_changed(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeFirst',
             'first_name': 'Jack'
         })
 
         # page should be redirected
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, 'First name cannot be empty. Enter your new first name.')
+        self.assertContains(response,'Successfully changed your first name.')
+
         find_user = User.objects.filter(username='testuser').first()
         self.assertTrue(find_user.first_name == 'Jack')  # should be changed.
 
     def test_last_name_changed(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeLast',
             'last_name': 'Jackson'
         })
 
         # page should be redirected
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, 'Last name cannot be empty. Enter your new last name.')
-        find_user = User.objects.filter(username='testuser').first()
-        self.assertTrue(find_user.last_name == 'Jack')  # should be changed.
+
+        self.assertTrue(self.user.last_name != 'Jackson')  # should be changed.
 
     # username checks
     def test_username_changed(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeUsername',
             'username': 'JoeBiden'
         })
 
         # page should be redirected
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, 'Username cannot be empty. '
                                       'Enter a valid new username.')
         self.assertNotContains(response, 'Username should be atleast 5 characters. Enter a valid new username.')
-        find_user = User.objects.filter(username='testuser').first()
-        self.assertTrue(find_user.username == 'JoeBiden')# should be changed.
+        self.assertContains(response, 'Username successfully changed.')
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.username == 'JoeBiden')# should be changed.
 
-    # password checks
-    # no password at all
+    # password check
     def test_Password_changed(self):
-        client = Client()
-        response = client.post('/settings/', {
-            'password': 'greatest23'
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changePass',
+            'oldPass': 'password',
+            'newPass': 'greatest23'
         })
 
         # page should be redirected
-        self.assertEqual(response.status_code, 302)
-        self.assertNotContains(response, 'Password cannot be blank. Enter a valid new password.')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'Old password cannot be empty. Please enter your old password.')
+        self.assertNotContains(response, 'New password cannot be blank. Please enter a valid new password.')
         self.assertNotContains(response,
                             'Password too short! Please enter a new password that is at least 8 characters.')
         self.assertNotContains(response,
                             'Password too long! Please enter a new password that is at most 24 characters.')
-        find_user = User.objects.filter(username='testuser').first()
-        self.assertTrue(find_user.password == 'greatest23')  # should be changed.
+        self.assertNotContains(response,
+                               'Old password does not match our records. Please re-enter your old password.')
+        self.assertContains(response,'Password successfully changed.')
+        self.user.refresh_from_db()
+
+        self.assertTrue(self.user.check_password('greatest23'))  # should be changed.
     # email checks
     def test_Email_changed(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeEmail',
             'email': 'jimmy@gmail.com',
         })
 
         # page should be redirected
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, 'Email cannot be empty. Enter a valid email.')
         self.assertNotContains(response, 'Invalid email address. Enter a valid email address.')
         self.assertNotContains(response, 'This email is already associated with another account. '
                                       'Please enter a different email address.')
-        find_user = User.objects.filter(username='testuser').first()
-        self.assertTrue(find_user.email == 'jimmy@gmail.com') # should be changed.
+        self.assertContains(response, 'Email successfully changed.')
 
+        self.user.refresh_from_db() # refresh the database
 
-    # Not entering an date of birth
+        self.assertFalse(self.user.email == 'email@gmail.com')
+        self.assertTrue(self.user.email == 'jimmy@gmail.com') # should be changed.
+
     def test_BirthDate_changed(self):
-        client = Client()
-        response = client.post('/settings/', {
+        response = self.client.post(reverse('settings', args=[self.user.id]), {
+            'action': 'changeDate',
             'birthDate': '1972-01-05'
         })
 
         # page should be rendered or reloaded as settings was invalid.
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, 'Please enter your date of birth.')
-        find_user = User.objects.filter(username='testuser').first()
-        self.assertTrue(find_user.birthDate == '1972-01-05') # should be changed.
+        self.assertContains(response, 'Date of birth successfully changed.')
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.birthDate, datetime.date(1972, 1, 5)) # should be changed.
 
